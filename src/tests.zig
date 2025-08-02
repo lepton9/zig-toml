b2cac707f3ecf70a67009644da567a50ea12aa7e
lepton <tonihirvonen00@gmail.com>
2025-08-03 02:49:08 +0300

    key tests

--- src/tests.zig
+++ src/tests.zig
@@ -19,4 +19,30 @@ test "parser" {
     };
     defer toml_data.deinit();
     toml_data.table.print();
+
+test "keys" {
+    const p = try parser.Parser.init(std.testing.allocator);
+    defer p.deinit();
+    const toml_data = try p.parse_string(
+        \\ key = "value"
+        \\ bare_key = "value"
+        \\ bare-key = "value"
+        \\ 1234 = "value"
+        \\ "127.0.0.1" = "value"
+        \\ "character encoding" = "value"
+        \\ "ʎǝʞ" = "value"
+        \\ 'key2' = "value"
+        \\ 'quoted "value"' = "value"
+        \\ "" = "blank"
+        \\ '' = 'blank'
+        \\ name = "Orange"
+        \\ physical.color = "orange"
+        \\ physical.shape = "round"
+        \\ site."google.com" = true
+        \\ fruit.name = "banana"
+        \\ fruit. color = "yellow"
+        \\ fruit . flavor = "banana"
+        \\ 3.14159 = "pi"
+    );
+    defer toml_data.deinit();
 }
